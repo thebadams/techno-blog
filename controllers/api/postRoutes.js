3c8cdb93e3ff71a64d73b729cc57a304755773b2
Brian Adams <bpeteradams@gmail.com>
2021-05-10 17:08:21 -0400

    developed api route to post new post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -13,4 +13,13 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const post = await Post.create(req.body);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
