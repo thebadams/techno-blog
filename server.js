5b75beee84c30faaca985dbce19d07b0579d76b3
Brian Adams <bpeteradams@gmail.com>
2021-05-10 11:19:31 -0400

    basic express server set up

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
