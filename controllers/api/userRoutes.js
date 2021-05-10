2b4e5c2425af623f0d2eea045e76906290b56f20
Brian Adams <bpeteradams@gmail.com>
2021-05-10 16:52:03 -0400

    developed post request for creating a new user

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.findAll();
+    if (!users) {
+      res.status(400).json({ message: 'No Users Found!' });
+    }
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
