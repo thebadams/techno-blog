bb52ecccde92cf5c057f3eedaf9b3387d77f6c65
Brian Adams <bpeteradams@gmail.com>
2021-05-10 11:57:52 -0400

    wrote out a Post model for use in sequelize

--- models/index.js
+++ models/index.js
@@ -1,3 +1,12 @@
 const { User } = require('./User');
+const { Post } = require('./Post');
 
-module.exports = User;
+User.hasMany(Post, {
+  foreignKey: 'creator_id',
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'creator_id',
+});
+
+module.exports = { User, Post };
