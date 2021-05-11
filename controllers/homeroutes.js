38c4e344a64670210163451fc777d245d8d9b446
Brian Adams <bpeteradams@gmail.com>
2021-05-11 12:51:51 -0400

    wrote basic functionality to display all posts on main page

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
 const auth = require('../utils/auth');
+const { Post, User } = require('../models');
 
 router.get('/', async (req, res) => {
-  console.log(res.session);
-  res.render('homepage');
+  const postsData = await Post.findAll({
+    include: [
+      {
+        model: User,
+      },
+    ],
+  });
+  const posts = postsData.map((post) => post.get({ plain: true }));
+  console.log(posts);
+  res.render('homepage', {
+    posts,
+  });
 });
 
 router.get('/login', async (req, res) => {
