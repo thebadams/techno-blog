40b2f2ae763f10d192dc5b272608d315b9a4615d
Brian Adams <bpeteradams@gmail.com>
2021-05-11 14:27:37 -0400

    wrote logic to handle sign up

---
+++ public/js/signup.js
@@ -0,0 +1,25 @@
+const signupForm = document.querySelector('#sign-up-form');
+
+const signup = async (event) => {
+  event.preventDefault();
+  const userNameEl = document.querySelector('#user-name');
+  const passwordEl = document.querySelector('#password');
+  
+  const userName = userNameEl.value.trim();
+  const password = passwordEl.value;
+
+  if (userName && password) {
+    const response = await fetch('/api/users/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application.json' },
+      body: JSON.stringify({ userName, password }),
+    });
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+signupForm.addEventListener('submit', signup);
