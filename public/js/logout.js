96121097ab1b55942b662c0fbc52fe6fb7a7d937
Brian Adams <bpeteradams@gmail.com>
2021-05-11 14:05:44 -0400

    developed logout logic

---
+++ public/js/logout.js
@@ -0,0 +1,14 @@
+const logoutLink = document.querySelector('#logout');
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+logoutLink.addEventListener('click', logout);
